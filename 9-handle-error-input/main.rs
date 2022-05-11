2c2df486b219805e30948e5becfd8624bc32211d
Karthic Rao <kartronics85@gmail.com>
2022-05-11 13:08:07 +0000

    - Adding graceful error handling for the parse function.

--- 9-handle-error-input/main.rs
+++ 9-handle-error-input/main.rs
@@ -7,17 +7,23 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
-
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
+        // --snip--
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
 
         // --snip--
 
