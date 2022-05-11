cbe5b1ef91fc482668cbad313a6edd01ddc0ab81
Karthic Rao <kartronics85@gmail.com>
2022-05-11 11:30:07 +0000

    Adding loop construct

---
+++ 8-looping/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+  match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+  }
+}
