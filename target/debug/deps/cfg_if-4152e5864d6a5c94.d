d49dacb05f9c112561f9d78328e2338ee211d71b
Karthic Rao <kartronics85@gmail.com>
2022-05-11 10:48:06 +0000

    Removing the target folder

--- target/debug/deps/cfg_if-4152e5864d6a5c94.d
+++
@@ -1,7 +0,0 @@
-/home/runner/Static-typing-trim-and-parse-in-Rustlang/target/debug/deps/cfg_if-4152e5864d6a5c94.rmeta: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/runner/Static-typing-trim-and-parse-in-Rustlang/target/debug/deps/libcfg_if-4152e5864d6a5c94.rlib: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/runner/Static-typing-trim-and-parse-in-Rustlang/target/debug/deps/cfg_if-4152e5864d6a5c94.d: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
--- target/debug/episode-7-trim-parse.d
+++
@@ -1,1 +0,0 @@
-/home/runner/Static-typing-trim-and-parse-in-Rustlang/target/debug/episode-7-trim-parse: /home/runner/Static-typing-trim-and-parse-in-Rustlang/7-trim-parse/main.rs
